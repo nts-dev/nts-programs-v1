83bf686fc9b5b2428b348a620744ad920cfe8aac
nts-dev <daniel@nts.nl>
2021-04-15 18:14:43 +0300

    config fix

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ ini_set('display_errors', '1');
 /* Default time zone ,to be able to send mail */
 date_default_timezone_set('UTC');
 
-require_once 'includes.php';
+include_once 'includes.php';
 
 $dbc = mysqli_connect(Boot::DBHOST, Boot::DBUSER, Boot::DBPASS, Boot::DBNAME);
 
