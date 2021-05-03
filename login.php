8b3febaaff586c9d3985cb56d88a7a8c5ef39f5a
nts-dev <daniel@nts.nl>
2021-05-03 12:37:19 +0300

    login form styling

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_SESSION['ERRORS']))
             unset($_SESSION["ERRORS"]);
 
-       $isAuthenticated =  $service->authenticateClient(
+        $isAuthenticated = $service->authenticateClient(
             filter_input(INPUT_POST, 'trainee', FILTER_SANITIZE_NUMBER_INT),
             filter_input(INPUT_POST, 'identifier', FILTER_SANITIZE_STRING)
         );
@@ -50,42 +50,78 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <?php
     CSSPackage::BOOTSTRAP();
+    CSSPackage::FONTAWESOME();
+
+    JSPackage::BOOTSTRAP();
+    JSPackage::JQUERY();
 
     ?>
 
     <style>
-        body {
-            font: 14px sans-serif;
+        .container {
+            border: 1px solid lightgrey;
+            text-align: center;
+            height: 300px;
+            width: 400px;
+            border-radius: 4px;
         }
 
-        .wrapper {
-            width: 350px;
-            padding: 20px;
+        body {
+            padding: 70px;
+        }
+
+        .input-group-text {
+            height: 100% !important;
+        }
+
+        h1 {
+            margin: auto;
+        }
+
+        .row {
+            height: 50px;
+            width: 396px;
+        }
+
+        .login-text {
+            padding-top: 22px;
         }
     </style>
 </head>
 <body>
-<div class="wrapper col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
-    <h2>Sign In</h2>
+<div class="container">
+    <div class="row login-text">
+        <h3><i class="fa fa-lock" aria-hidden="true"></i> Login</h3>
+    </div>
+    <br/><br/>
+
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div class="form-group text-left">
-            <label>Username</label>
-            <input type="text" name="trainee"
-                   class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
-                   value="<?php echo $username; ?>">
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fas fa-user-tie"></i></span>
+            </div>
+            <input
+                    type="text"
+                    name="trainee"
+                    class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+                    value="<?php echo $username; ?>"
+                    placeholder="username"/>
             <span class="invalid-feedback"><?php echo $username_err; ?></span>
         </div>
-        <div class="form-group text-left">
-            <label>Password</label>
-            <input type="password" name="identifier"
+        <br/>
+
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-key icon"></i></span>
+            </div>
+            <input type="Password" name="identifier"
                    class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
-                   value="<?php echo $password; ?>">
+                   value="<?php echo $password; ?>"
+                   placeholder="password"/>
             <span class="invalid-feedback"><?php echo $password_err; ?></span>
         </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Submit">
-            <span class="invalid-feedback"><?php echo $login_err; ?></span>
-        </div>
+        <br/>
+        <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-off"></span> Login</button>
     </form>
 </div>
 </body>
